c8fae1780ef42408a1b32fa3101e0d5e24d082c5
heejin han <heejinhan3@gmail.com>
2021-08-25 12:29:37 +0900

    conditional

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,18 @@
-const player = {
-    name : "coco",
-    points : 100,
-    fat : true ,
-};
-
-console.log(player);
-console.log(player.fat);
-console.log(player[points]);
+const age =parseInt(prompt("how old are you?"));
 
 
+console.log(typeof age);
+console.log(isNaN(age));
+console.log(age);
+
+
+
+if(isNaN(age)){
+    console.log("please write number");
+}else if (age>=18 && age<=50){
+    console.log("you can drink");
+}else if( age >50 && age<=80){
+    console.log("you have to exercise");
+}else{
+    console.log("you do whatever you want");
+}
